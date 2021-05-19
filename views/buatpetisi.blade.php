15602a56f8e81dbafa34e7974c86fbf59ee7950b
KuproyMalang <82764920+KuproyMalang@users.noreply.github.com>
2021-05-19 12:58:46 +0700

    hal hal yang harus diketahui untuk buat petisi

--- views/buatpetisi.blade.php
+++ views/buatpetisi.blade.php
@@ -1,7 +1,26 @@
 @extends('layout.guest')
 <title>Buat Petisi</title>
-@section('content')
 
+
+@section('content')
+<div class="container" style="text-align: center">
+    <h1 style="color: red"><b>Membuat Petisi</b></h1>
+</div>
+<br>
+<br>
+<div class="container" style="text-align: center">
+    <h3 style="color: red">Hal hal yang harus anda inginkan ketika membuat petisi</h3>
+</div>
+<br>
+<div class="container" style="text-align:center">
+    <h4>Isu apa yang ada ingin buat?</h4>
+    <h4>Mengapa anda membuat petisi tersebut?</h4>
+    <h4>Kemanakah Tujuan anda membuat?</h4>
+</div>
+<br>
+<div style="text-align: center">
+    <a href="/buatpetisi2" type="button" class="btn btn-success"> Mari Buat Petisi</a>
+</div>
 
 
 
