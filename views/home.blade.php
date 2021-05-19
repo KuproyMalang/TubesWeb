788482fe7e3babbf252bac171d72ea54f304aa87
KuproyMalang <82764920+KuproyMalang@users.noreply.github.com>
2021-05-19 12:28:20 +0700

    selamat datang petisi

--- views/home.blade.php
+++ views/home.blade.php
@@ -1,7 +1,13 @@
 @extends('layout.guest')
 <title>Home</title>
 @section('content')
-
+<div style="text-align: center">
+    <h1 style="color:grey" >Selamat Datang di Petisi Kita</h1>
+</div>
+<br>
+<div style="text-align: center">
+    <a href="/buatpetisi" type="button" class="btn btn-success">Buat Petisi</a>
+</div>
 
 
 
