1900fda095216627581281d3aef5af938b7e4db2
KuproyMalang <82764920+KuproyMalang@users.noreply.github.com>
2021-05-27 01:25:05 +0700

    routes

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,38 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/admin/home', function () {
+Route::get('/admin', function () {
     return view('layout.master');
+});
+
+Route::get('/guest', function () {
+    return view('layout.guest');
 });
 
 Route::get('/login', function () {
     return view('login');
+});
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/buatpetisi', function () {
+    return view('buatpetisi');
+});
+
+Route::get('/buatpetisi2', function () {
+    return view('buatpetisi2');
+});
+
+Route::get('/buatakun', function () {
+    return view('buatakun');
+});
+
+Route::get('/telusuri', function () {
+    return view('telusuri');
+});
+
+Route::get('/petisisaya', function () {
+    return view('petisisaya');
 });
